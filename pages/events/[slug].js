c365f34485dcfaf82298b91bb780d2290374d2d8
saloos93 <milad.emami1025@gmail.com>
2021-10-25 19:56:58 +0330

    create layout header footer

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,11 @@
+import { useRouter } from "next/router";
+import Layout from "../../components/Layout";
 export default function EventPage() {
+  const router = useRouter();
+  console.log(router);
   return (
-    <div>
+    <Layout>
       <h1>My Event page</h1>
-    </div>
+    </Layout>
   );
 }
