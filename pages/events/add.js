d29dba969f53295f2ef1f1178a6bee4ad28127ef
saloos93 <milad.emami1025@gmail.com>
2021-10-25 19:57:26 +0330

    header footer

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,7 +1,9 @@
+import Layout from "../../components/Layout";
+
 export default function AddPage() {
   return (
-    <div>
+    <Layout title="Add new events">
       <h1>Add Event</h1>
-    </div>
+    </Layout>
   );
 }
